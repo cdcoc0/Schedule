3cb83ef99d155e5b5770be7430fb4c908981cd3b
velykitty <jiyunee13@gmail.com>
2021-05-08 15:13:17 +0900

    Clock.js -renders clock in every minute -calls getTime method in every minute using useEffect

--- src/momentum/Clock.js
+++ src/momentum/Clock.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from 'styled-components';
 
 const ClockContainer = styled.div`
@@ -6,13 +6,30 @@ const ClockContainer = styled.div`
 
 const Clock = () => {
     const date = new Date();
-    // const [hours, setHours] = useState(date.getHours());
-    // const [minutes, setMinutes] = useState(date.getMinutes());
-    const [time] = useState({
+    const [time, setTime] = useState({
         hours: date.getHours(),
         minutes: date.getMinutes(),
-        seconds: date.getSeconds()
     });
+
+    const getTime = () => {
+        setTime({hours: date.getHours(), minutes: date.getMinutes()});
+    }
+
+    useEffect(() => {
+        const oneMinCall =  setInterval(getTime(), 6000);
+        return clearInterval(oneMinCall);
+    }, [time]);
+   
+    // const getTime = () => {
+    //     const date = new Date();
+    //     setTime({
+    //         hours: date.getHours(),
+    //         minutes: date.getMinutes(),
+    //         //seconds: date.getSeconds()
+    //     });
+
+    //     return `${time.hours}:${time.minutes}`;
+    // };
 
     return (
         <ClockContainer>
