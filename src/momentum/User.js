289b87e42de7b93dfa2d69176e120bd4a123638e
velykitty <jiyunee13@gmail.com>
2021-05-10 10:44:24 +0900

    Greeting -save username in the local storage -apply useCallback

--- src/momentum/User.js
+++ src/momentum/User.js
@@ -1,23 +1,40 @@
-import React, {useState} from 'react';
+import React, {useState, useCallback} from 'react';
 import styled from 'styled-components';
 
 const UserContainer = styled.div`
 `
 
-const User = () => {    
-    const loadUser = () => {
-        const USER_KEY = "User";
+const User = () => {   
+    const USER_KEY = "User"; 
+    const [userInput, setUserInput] = useState("");
+
+    const onChange = useCallback (
+        e => {
+            setUserInput(e.target.value);
+            }, []);
+
+    const onSubmit = useCallback (
+        () => {
+            //e.preventDefault();
+            //새로고침 안할 수 있는 방법 찾아보기 visible true/false
+            localStorage.setItem(USER_KEY, userInput);
+            //setUserInput("");
+            }, [userInput]);
+
+    const loadUser = useCallback (
+        () => {
         const USER_VAL = localStorage.getItem(USER_KEY);
         if(USER_VAL === "") {
             return (
-                <form>
-                    <input type="text" placeholder="Name"></input>
+                <form  onSubmit={onSubmit}>
+                    <input type="text" placeholder="Name" 
+                    value={userInput} onChange={onChange} />
                     <button>enter</button>
                 </form>
             );
         }
         return (<h4>{`Good Day ${USER_VAL}!`}</h4>);
-    };
+    }, [onChange, onSubmit, userInput]);
 
     return (
         <UserContainer>
