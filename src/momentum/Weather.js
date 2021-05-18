df7792554e129c81bd663b9b76c4305126832819
velykitty <jiyunee13@gmail.com>
2021-05-18 17:48:26 +0900

    get wether information

--- src/momentum/Weather.js
+++ src/momentum/Weather.js
@@ -1,73 +1,75 @@
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 
-const getWeather = (lat, lon) => {
-    const API_KEY = "bae1076bc4c9ce19a1bd0aa7f04a7d96";
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`).then(response => {
-        return response.json();
-    }).then(json => {
-        const result = {
-            place: json.name,
-            temperature: json.main.temp,
-            description: json.weather[0].main
-        };
-        console.log(result);
-        return result;
-    });
-};
-
-const saveCoords = (coordsObj) => {
-    localStorage.setItem("COORDS", JSON.stringify(coordsObj));
-};
-
-const geoSuccess = (position) => {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-    const coordsObj = {
-        latitude,
-        longitude
-    };
-    saveCoords(coordsObj);
-    getWeather(latitude, longitude);
-};
-
-const geoErr = () => {
-    console.log("geolocation error");
-};
-
-const getCoords = () => {
-    navigator.geolocation.getCurrentPosition(geoSuccess, geoErr);
-}; 
 
 const Weather = () => {
-    // const [weather, setWeather] = useState();
+    const [weather, setWeather] = useState({
+        place: 'Seoul, Korea',
+        temperature: 0,
+        description: 'Clear'
+    });
 
-    const loadCoords = () => {
+    const getWeather = useCallback((lat, lon) => {
+        const API_KEY = "bae1076bc4c9ce19a1bd0aa7f04a7d96";
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`).then(response => {
+            return response.json();
+        }).then(json => {
+            const result = {
+                place: json.name,
+                temperature: Math.floor(json.main.temp),
+                description: json.weather[0].main
+            };
+            console.log(json);
+            setWeather({
+                place: result.place,
+                temperature: result.temperature,
+                description: result.description
+            });
+        });
+    }, []);
+    
+    const saveCoords = useCallback(coordsObj => {
+        localStorage.setItem("COORDS", JSON.stringify(coordsObj));
+    }, []);
+    
+    const geoSuccess = useCallback(position => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        const coordsObj = {
+            latitude,
+            longitude
+        };
+        saveCoords(coordsObj);
+        getWeather(latitude, longitude);
+    }, []);
+    
+    const geoErr = useCallback(() => {
+        console.log("geolocation error");
+    }, []);
+    
+    const getCoords = useCallback(() => {
+        navigator.geolocation.getCurrentPosition(geoSuccess, geoErr);
+    }, []);
+    
+    const loadCoords = useCallback(() => {
         const Coords = localStorage.getItem("COORDS");
         if(Coords === "") {
             console.log("COORDS 가져오는 중");
             getCoords();
         } else {
             const parsedCoords = JSON.parse(Coords);
-            const weather = getWeather(parsedCoords.latitude, parsedCoords.longitude);
-            console.log(weather);
+            getWeather(parsedCoords.latitude, parsedCoords.longitude);
         }
-    };   
-
+    },[]);   
+    
     useEffect(() => {
         loadCoords();
-        // setWeather({
-        //     place: loadCoords.place,
-        //     temperature: loadCoords.temperature,
-        //     description: loadCoords.description
-        // });
-        //console.log();
     }, []);
 
     return (
         <div>
-            {/* <span>{weather.place}</span>
-            <span>{weather.temperature}</span>
-            <span>{weather.description}</span> */}
+            <div>{`@${weather.place}`}</div>
+            <span>{`${weather.temperature}°C `}</span>
+            <span>{weather.description}</span>
         </div>
     );
 };
