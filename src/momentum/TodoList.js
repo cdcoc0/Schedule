676b064150c89501ec8b40793a13f9da1b9d7f34
velykitty <jiyunee13@gmail.com>
2021-05-25 02:04:11 +0900

    arrange todo item

--- src/momentum/TodoList.js
+++ src/momentum/TodoList.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 import TodoItem from './TodoItem';
-import './styles/TodoList.scss';
+
+const ListContainer = styled.div`
+    min-height: 50px;
+    max-height: 320px;
+    overflow-y: auto;
+    //background-color: white;
+`
 
 const TodoList = ({todos, onRemove, onToggle}) => {
     return(
-        <div>
+        <ListContainer>
             {todos && todos.map(todo => <TodoItem key={todo.id} todo={todo} onRemove={onRemove} onToggle={onToggle}/>)}
-        </div>
+        </ListContainer>
     );
 };
 
